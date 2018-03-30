7a17fa751d248aa04518e17c61fede57b6f4ae29
m2wasabi <m2wasabi@gmail.com>
2018-03-30 18:58:50 +0900

    クロマキー機能追加

---
+++ Assets/FaceTrack/Script/BackGroundChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundChanger : MonoBehaviour {
+
+public GameObject BackGroundObject;
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            BackGroundObject.SetActive(!BackGroundObject.activeSelf);
+        }
+	}
+}
