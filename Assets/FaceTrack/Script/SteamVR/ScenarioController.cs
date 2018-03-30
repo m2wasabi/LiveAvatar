bfd7b925b295c08ce7aef9ec4621ca14b0ed121e
m2wasabi <m2wasabi@gmail.com>
2018-03-30 12:58:21 +0900

    ScenarioController: Vive コントローラーで台詞選択

--- Assets/FaceTrack/Script/SteamVR/ScenarioController.cs
+++ Assets/FaceTrack/Script/SteamVR/ScenarioController.cs
@@ -9,6 +9,7 @@ public class ScenarioController : MonoBehaviour {
     private SteamVR_Controller.Device _device_L, _device_R;
 
     public GameObject ScrollViewUI;
+    private ScenarioLoader _scenarioLoader;
 
     void Start () {
         if (leftHandController != null)
@@ -21,12 +22,35 @@ public class ScenarioController : MonoBehaviour {
             var controller_r = rightHandController.GetComponent<SteamVR_TrackedObject>();
             _device_R = SteamVR_Controller.Input((int)controller_r.index);
         }
+        _scenarioLoader = GetComponent<ScenarioLoader>();
     }
 
     void Update () {
         if (_device_L.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu) && !_device_R.GetPress(SteamVR_Controller.ButtonMask.ApplicationMenu))
         {
             ScrollViewUI.SetActive(!ScrollViewUI.activeSelf);
+        }
+
+        if (_device_L.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad) && !_device_L.GetPress(SteamVR_Controller.ButtonMask.Grip))
+        {
+            var touchPos = _device_L.GetAxis();
+
+            if (Mathf.Abs(touchPos.x) < Mathf.Abs(touchPos.y))
+            {
+                if (touchPos.y > 0.3)
+                {
+                    _scenarioLoader.MoveUp();
+                }
+                else if (touchPos.y < -0.3)
+                {
+                    _scenarioLoader.MoveDOwn();
+                }
+            }
+        }
+
+        if (_device_L.GetPressDown(SteamVR_Controller.ButtonMask.Trigger) && ScrollViewUI.activeSelf)
+        {
+            _scenarioLoader.ToggleAction();
         }
 
     }
