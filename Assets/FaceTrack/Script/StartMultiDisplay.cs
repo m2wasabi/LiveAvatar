3ac0498b3b2c178af51ab90a35832bb6f0e2da66
m2wasabi <m2wasabi@gmail.com>
2018-03-30 14:12:19 +0900

    マルチディスプレイ対応

---
+++ Assets/FaceTrack/Script/StartMultiDisplay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+
+public class StartMultiDisplay : MonoBehaviour {
+
+
+
+	void Start () 
+
+	{
+
+		int maxDisplayCount = 2;
+
+		for (int i=0; i<maxDisplayCount && i<Display.displays.Length; i++) {
+
+			Display.displays[i].Activate();
+
+		}
+
+	}
+
+}
