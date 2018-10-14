8d796f92ebee07d7ada5b8f1bd0618a0b72132e1
m2wasabi <m2wasabi@gmail.com>
2018-10-15 00:05:40 +0900

    Add UI Code VRMPreviewWindow

---
+++ Assets/LiveAvatar/AvatarController/VRM/VRMPreviewUI.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using VRM;
+
+namespace LiveAvatar.AvatarController.VRM
+{
+    public class VRMPreviewUI : MonoBehaviour {
+        [Serializable]
+        struct TextFields
+        {
+            [SerializeField, Header("Info")]
+            Text m_textModelTitle;
+            [SerializeField]
+            Text m_textModelVersion;
+            [SerializeField]
+            Text m_textModelAuthor;
+            [SerializeField]
+            Text m_textModelContact;
+            [SerializeField]
+            Text m_textModelReference;
+            [SerializeField]
+            RawImage m_thumbnail;
+
+            [SerializeField, Header("CharacterPermission")]
+            Text m_textPermissionAllowed;
+            [SerializeField]
+            Text m_textPermissionViolent;
+            [SerializeField]
+            Text m_textPermissionSexual;
+            [SerializeField]
+            Text m_textPermissionCommercial;
+            [SerializeField]
+            Text m_textPermissionOther;
+
+            [SerializeField, Header("DistributionLicense")]
+            Text m_textDistributionLicense;
+            [SerializeField]
+            Text m_textDistributionOther;
+
+            public void Start()
+            {
+                m_textModelTitle.text = "";
+                m_textModelVersion.text = "";
+                m_textModelAuthor.text = "";
+                m_textModelContact.text = "";
+                m_textModelReference.text = "";
+
+                m_textPermissionAllowed.text = "";
+                m_textPermissionViolent.text = "";
+                m_textPermissionSexual.text = "";
+                m_textPermissionCommercial.text = "";
+                m_textPermissionOther.text = "";
+
+                m_textDistributionLicense.text = "";
+                m_textDistributionOther.text = "";
+            }
+
+            public void UpdateMeta(VRMMetaObject meta)
+            {
+                m_textModelTitle.text = meta.Title;
+                m_textModelVersion.text = meta.Version;
+                m_textModelAuthor.text = meta.Author;
+                m_textModelContact.text = meta.ContactInformation;
+                m_textModelReference.text = meta.Reference;
+
+                m_textPermissionAllowed.text = meta.AllowedUser.ToString();
+                m_textPermissionViolent.text = meta.ViolentUssage.ToString();
+                m_textPermissionSexual.text = meta.SexualUssage.ToString();
+                m_textPermissionCommercial.text = meta.CommercialUssage.ToString();
+                m_textPermissionOther.text = meta.OtherPermissionUrl;
+
+                m_textDistributionLicense.text = meta.LicenseType.ToString();
+                m_textDistributionOther.text = meta.OtherLicenseUrl;
+
+                if(meta.Thumbnail)
+                    m_thumbnail.texture = meta.Thumbnail;
+            }
+        }
+        [SerializeField]
+        TextFields m_texts;
+
+        // Use this for initialization
+        void Start () {
+		
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+    }
+}
