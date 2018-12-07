4c91f2913be1bacce994dcfce9db041e7d3eaeaa
m2wasabi <m2wasabi@gmail.com>
2018-12-07 10:49:27 +0900

    BackGroundChanger Update

--- Assets/LiveAvatar/Script/BackGroundChanger.cs
+++ Assets/LiveAvatar/Script/BackGroundChanger.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class BackGroundChanger : MonoBehaviour {
 
-public GameObject BackGroundObject;
-	void Start () {
-		
-	}
-	
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.G))
+    [SerializeField]
+    private List<Camera> Cameras;
+    [SerializeField]
+    private List<GameObject> BackGroundObjects;
+
+    void SetObjectOff()
+    {
+        foreach (var bgo in BackGroundObjects)
         {
-            BackGroundObject.SetActive(!BackGroundObject.activeSelf);
+            bgo.SetActive(false);
         }
-	}
+    }
+
+    void SetBGColor(Color color)
+    {
+        foreach (var camera in Cameras)
+        {
+            camera.backgroundColor = color;
+        }
+        SetObjectOff();
+    }
+
+    void SetBGObject(int index)
+    {
+        SetObjectOff();
+        BackGroundObjects[index].SetActive(true);
+    }
+
+    public void OnChangeSetting(int index)
+    {
+        switch (index)
+        {
+                case 0:
+                    SetBGObject(0);
+                    break;
+                case 1:
+                    SetBGColor(Color.green);
+                    break;
+                case 2:
+                    SetBGColor(Color.white);
+                    break;
+                default:
+                    break;
+        }
+    }
 }
