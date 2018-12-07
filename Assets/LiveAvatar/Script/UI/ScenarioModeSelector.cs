6468580cd70c81179917a45810388345e8034609
m2wasabi <m2wasabi@gmail.com>
2018-12-07 22:05:22 +0900

    Config ScenarioSelector

---
+++ Assets/LiveAvatar/Script/UI/ScenarioModeSelector.cs
@@ -0,0 +1,28 @@
+﻿using LiveAvatar.Speech;
+using UnityEngine;
+
+namespace LiveAvatar.UI
+{
+    public class ScenarioModeSelector : MonoBehaviour
+    {
+        [SerializeField]
+        private ScenarioController _scenarioController;
+        [SerializeField]
+        private SpeechRecognizer _speechRecognizer;
+
+        public void SetScenarioMode(int index)
+        {
+            switch (index)
+            {
+                    case 0:
+                        _speechRecognizer.SetEnabled(false);
+                        _scenarioController.SetEnabled(true);
+                        break;
+                    case 1:
+                        _scenarioController.SetEnabled(false);
+                        _speechRecognizer.SetEnabled(true);
+                        break;
+            }
+        }
+    }
+}
