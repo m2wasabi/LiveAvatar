ac65aff7fc058c0328401017934d44c4bcfbc91a
m2wasabi <m2wasabi@gmail.com>
2018-12-07 16:22:39 +0900

    Steamコントロールを分離した BlendShapeController

--- Assets/LiveAvatar/AvatarController/VRM/BlendShapeController.cs
+++ Assets/LiveAvatar/AvatarController/VRM/BlendShapeController.cs
@@ -1,67 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using VRM;
 
 namespace LiveAvatar.AvatarController.VRM
 {
     public class BlendShapeController : MonoBehaviour
     {
-        public GameObject RightHandController;
-        private SteamVR_Controller.Device _device_R;
-        public Toggle FaceToggle;
-
+        public BlendShapeKey EmotionStat;
+        public float EmotionMag;
         public float MouthOpen { get; set; }
+        public float LeftEyeOpen { get; set; }
+        public float RightEyeOpen { get; set; }
 
         private VRMBlendShapeProxy m_blendShapePloxy;
 
-        private bool _edittingFace = false;
+        private List<KeyValuePair<BlendShapeKey, float>> m_faceReset;
 
-        void Start () {
-            if (RightHandController != null)
-            {
-                var controller_r = RightHandController.GetComponent<SteamVR_TrackedObject>();
-                _device_R = SteamVR_Controller.Input((int)controller_r.index);
-            }        
-        }
-	
         void Update () {
-            if (m_blendShapePloxy)
+            if (m_blendShapePloxy != null)
             {
-                if (FaceToggle.isOn)
-                {
-                    if (_device_R.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad) && !_device_R.GetPress(SteamVR_Controller.ButtonMask.Grip))
-                    {
-                        Debug.Log("Into Face Con");
-                        _edittingFace = true;
-                    }
-    
-                    if (_edittingFace && _device_R.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
-                    {
-                        Debug.Log("Out Face Con");
-                        _edittingFace = false;
-                    }
-                    if(_edittingFace) updateFacePose();
-                }
-                m_blendShapePloxy.SetValue(BlendShapePreset.A, MouthOpen);
+                UpdateFacePose();
             }
         }
 
         public void Setup()
         {
             m_blendShapePloxy = gameObject.GetComponentInChildren<VRMBlendShapeProxy>();
+            InitializeFaceReset();
         }
 
         public void Setup(VRMBlendShapeProxy blendShapeProxy)
         {
             m_blendShapePloxy = blendShapeProxy;
+            InitializeFaceReset();
         }
 
-        private void updateFacePose()
+        private void UpdateFacePose()
         {
-            var rightPos = _device_R.GetAxis();
-            var FaceReset = new List<KeyValuePair<BlendShapeKey, float>>
+            m_blendShapePloxy.SetValues(m_faceReset);
+            m_blendShapePloxy.SetValue(EmotionStat, EmotionMag);
+            m_blendShapePloxy.SetValue(BlendShapePreset.A, MouthOpen);
+            if (EmotionStat.Name == "NEUTRAL" || EmotionStat.Name == "ANGRY")
+            {
+                m_blendShapePloxy.SetValue(BlendShapePreset.Blink_L, 1.0f - LeftEyeOpen);
+                m_blendShapePloxy.SetValue(BlendShapePreset.Blink_R, 1.0f - RightEyeOpen);
+            }
+        }
+
+        private void InitializeFaceReset()
+        {
+            m_faceReset = new List<KeyValuePair<BlendShapeKey, float>>
             {
                 new KeyValuePair<BlendShapeKey, float>(new BlendShapeKey(BlendShapePreset.A),0.0f),
                 new KeyValuePair<BlendShapeKey, float>(new BlendShapeKey(BlendShapePreset.I),0.0f),
@@ -73,41 +61,15 @@ namespace LiveAvatar.AvatarController.VRM
                 new KeyValuePair<BlendShapeKey, float>(new BlendShapeKey(BlendShapePreset.Joy),0.0f),
                 new KeyValuePair<BlendShapeKey, float>(new BlendShapeKey(BlendShapePreset.Blink),0.0f),
                 new KeyValuePair<BlendShapeKey, float>(new BlendShapeKey(BlendShapePreset.Sorrow),0.0f),
+                new KeyValuePair<BlendShapeKey, float>(new BlendShapeKey(BlendShapePreset.Blink_L),0.0f),
+                new KeyValuePair<BlendShapeKey, float>(new BlendShapeKey(BlendShapePreset.Blink_R),0.0f),
             };
-            m_blendShapePloxy.SetValues(FaceReset);
-    
-            var rightDeg = Vector2.Angle(new Vector2(-1, 0), rightPos);
-            var faceMag = rightPos.magnitude;
-            if (rightPos.y < 0)
-            {
-                rightDeg = 360 - rightDeg;
-            }
-    
-            if (rightDeg >= 30 && rightDeg < 90)
-            {
-                // Smile
-                m_blendShapePloxy.SetValue(BlendShapePreset.Joy, faceMag);
-            }
-            else if (rightDeg >= 90 && rightDeg < 150)
-            {
-                // Angry
-                m_blendShapePloxy.SetValue(BlendShapePreset.Angry, faceMag);
-            }
-            else if (rightDeg >= 150 && rightDeg < 210)
-            {
-                // Sorrow
-                m_blendShapePloxy.SetValue(BlendShapePreset.Sorrow, faceMag);
-            }
-            else if (rightDeg >= 210 && rightDeg < 270)
-            {
-                // EyeClose
-                m_blendShapePloxy.SetValue(BlendShapePreset.Blink, faceMag);
-            }
-            else if (rightDeg >= 270 && rightDeg < 330)
-            {
-                // Fun
-                m_blendShapePloxy.SetValue(BlendShapePreset.Fun, faceMag);
-            }
+        }
+
+        public void SetFaceExpression(BlendShapeKey blendShapeKey, float magnitude)
+        {
+            EmotionStat = blendShapeKey;
+            EmotionMag = magnitude;
         }
     }
 }
