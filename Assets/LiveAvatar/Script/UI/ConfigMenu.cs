14dd798509a64794e682cf87f59dadd6287bc3a7
m2wasabi <m2wasabi@gmail.com>
2018-12-07 12:11:58 +0900

    Spawn New Config UI

---
+++ Assets/LiveAvatar/Script/UI/ConfigMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+namespace LiveAvatar.UI
+{
+    public class ConfigMenu : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject ConfigUI;
+
+        void Start () {
+            this.UpdateAsObservable()
+                .Where(_ => Input.GetKeyDown(KeyCode.Escape))
+                .Subscribe(_ => ToggleUI());
+        }
+
+        public void ToggleUI()
+        {
+            ConfigUI.SetActive(!ConfigUI.activeSelf);
+        }
+    }
+}
