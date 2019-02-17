8d5cb103ced1d3a4b0388e603b54afce9d40657a
m2wasabi <m2wasabi@gmail.com>
2019-02-17 09:24:24 +0900

    カメラ移動

---
+++ Assets/LiveAvatar/Script/CameraSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private Transform camerapos;
+
+    public void SetCam1()
+    {
+        camerapos.position = Vector3.zero;
+        camerapos.rotation = Quaternion.identity;
+    }
+    public void SetCam2()
+    {
+        camerapos.position = new Vector3(0,0,-1.64f);
+        camerapos.rotation = Quaternion.identity;
+    }
+    public void SetCam3()
+    {
+        camerapos.position = new Vector3(0.36f,0.3f,-0.27f);
+        camerapos.rotation = Quaternion.identity;
+        camerapos.Rotate(0,31.61f,0);
+    }
+}
