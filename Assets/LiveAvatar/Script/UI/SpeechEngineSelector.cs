909470ed0a658989cc39e7bb34ca7b3b305d9605
m2wasabi <m2wasabi@gmail.com>
2019-02-18 09:55:22 +0900

    UI更新: SpeechEngine

---
+++ Assets/LiveAvatar/Script/UI/SpeechEngineSelector.cs
@@ -0,0 +1,32 @@
+﻿using LiveAvatar.Speech;
+using UnityEngine;
+
+namespace LiveAvatar.UI
+{
+    public class SpeechEngineSelector : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject m_seikaServerUI;
+        [SerializeField]
+        private GameObject m_remoteTalkUI;
+
+        public void SetSpeechEngine(int index)
+        {
+            switch (index)
+            {
+                    case 0:
+                        m_seikaServerUI.SetActive(false);
+                        m_remoteTalkUI.SetActive(false);
+                        break;
+                    case 1:
+                        m_seikaServerUI.SetActive(true);
+                        m_remoteTalkUI.SetActive(false);
+                        break;
+                    case 2:
+                        m_seikaServerUI.SetActive(false);
+                        m_remoteTalkUI.SetActive(true);
+                        break;
+            }
+        }
+    }
+}
